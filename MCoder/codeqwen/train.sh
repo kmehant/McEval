303454b060fe358a0b38e33311527451a3ba1990
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-07-08 20:48:35 +0530

    add
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- MCoder/codeqwen/train.sh
+++ MCoder/codeqwen/train.sh
@@ -1,15 +1,16 @@
-export MASTER_ADDR="localhost"
-export MASTER_PORT="1231"
-export GLOO_SOCKET_IFNAME="lo"
-export NCCL_SOCKET_IFNAME="lo"
+# export MASTER_ADDR="localhost"
+# export MASTER_PORT="1231"
+# export GLOO_SOCKET_IFNAME="lo"
+# export NCCL_SOCKET_IFNAME="lo"
 
 
-MODEL_PATH='CodeQwen1.5-7B'
+# MODEL_PATH='CodeQwen1.5-7B'
 
-DATA_PATH="<your data path>"
+# DATA_PATH="<your data path>"
 
-SAVE_PATH='./ckpt/sft-codewen-1.5-7b'
-CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" python -m torch.distributed.launch --master_addr ${MASTER_ADDR} --master_port ${MASTER_PORT} --nproc_per_node=8 --use_env train.py \
+# SAVE_PATH='./ckpt/sft-codewen-1.5-7b'
+# /data/odm_ase/train
+python -m torch.distributed.launch --master_addr ${MASTER_ADDR} --master_port ${MASTER_PORT} --nproc_per_node=${nproc_per_node} --use_env train.py \
     --model_name_or_path $MODEL_PATH \
     --data_path $DATA_PATH \
     --bf16 True \
